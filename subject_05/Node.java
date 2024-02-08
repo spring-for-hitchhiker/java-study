ffe9ae514e0c9ea93a87fbb1db307115d258ab51
KenWR <skyqkqk1168@gmail.com>
2024-02-08 14:02:10 +0900

    issue #5

---
+++ subject_05/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    int value;
+    Node left, right;
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
